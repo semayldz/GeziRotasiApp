69964ca88d4b676396083794a039c57337dd4d7d
sema <semaayildizz@hotmail.com>
2024-05-15 01:02:01 +0300

    Remove db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from app import app
-
-db = SQLAlchemy(app)
-login = LoginManager(app)
-
-login.login_view = 'login'  #giriş yapmamış kullanıcıları log in sayfasına yönlendirecek
-login.login_message_category = 'info'
